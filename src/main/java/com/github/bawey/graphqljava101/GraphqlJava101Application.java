edb3a51f3bb40ce28f3e312d8149636d333b70be
tbawej <tbawej@cern.ch>
2023-02-19 23:10:42 +0100

    Taking graphql-java tutorial for a spin

---
+++ src/main/java/com/github/bawey/graphqljava101/GraphqlJava101Application.java
@@ -0,0 +1,13 @@
+package com.github.bawey.graphqljava101;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlJava101Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphqlJava101Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/bawey/graphqljava101/GraphqlJava101ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.bawey.graphqljava101;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlJava101ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
